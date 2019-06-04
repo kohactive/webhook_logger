4c9c1403045e654f4f7c21e836003d2ec13cb28a
John Koht <john@kohactive.com>
2019-06-03 21:40:50 -0500

    add migrations to main app through engine

--- lib/webhook_logger/engine.rb
+++ lib/webhook_logger/engine.rb
@@ -8,5 +8,14 @@ module WebhookLogger
       g.assets false
       g.helper false
     end
+
+    # Run the engine migrations with the main app rake task
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
